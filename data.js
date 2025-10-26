93ac806ffa08c557b12772927bf5a3944d519656
thatonearchitect <a_bregadze2@cu.edu.ge>
2025-10-26 17:38:03 +0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,319 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "შემოსასვლელი",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5461247998542325,
+          "pitch": 0.4408833907639238,
+          "rotation": 0,
+          "target": "3--1"
+        },
+        {
+          "yaw": 1.2035487085335728,
+          "pitch": 0.46665770203786394,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": -0.12341825364962844,
+          "pitch": 0.424156260039398,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.4253393190772847,
+          "pitch": 0.27472894808246195,
+          "rotation": 0,
+          "target": "1---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1---",
+      "name": "მისაღები ოთახი სასადილო",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.138750552147897,
+          "pitch": 0.5284516629739464,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.46883525766978096,
+          "pitch": 0.2540711665045414,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.07893738805094586,
+          "pitch": 0.23168836789754899,
+          "rotation": 0,
+          "target": "5--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "სამზარეულო",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.498857127508165,
+          "pitch": 0.7364094529634144,
+          "rotation": 0,
+          "target": "1---"
+        },
+        {
+          "yaw": 1.4333651903510276,
+          "pitch": 0.39659775660421737,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--1",
+      "name": "საძინებელი 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3318123729090967,
+          "pitch": 0.6448112005399409,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.7600235454743398,
+          "pitch": 0.4490659750544772,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "გარდერობი",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0421479824250035,
+          "pitch": 0.8571385958102162,
+          "rotation": 0,
+          "target": "3--1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--2",
+      "name": "საძინებელი 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.281480426063281,
+          "pitch": 0.683232855300659,
+          "rotation": 0,
+          "target": "1---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-",
+      "name": "სააბაზანო",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8544454357977393,
+          "pitch": 0.9683051568268795,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
